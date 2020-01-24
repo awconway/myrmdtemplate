d790787baddcbfb249ff7c8e33a67f50d497ffe6
Aaron Conway <aaronconwayphd@gmail.com>
2020-01-24 11:48:02 -0500

    init template

---
+++ R/formats.R
@@ -0,0 +1,37 @@
+#' Output format for letter
+#'
+#' Each function is a wrapper for \code{\link[bookdown]{html_document2}} to
+#' produce documents in ConwayLetterTemplate style.
+#' @rdname template
+#' @param \dots Arguments passed to \code{\link[bookdown]{html_document2}}.
+#'
+#' @return An R Markdown output format object.
+#'
+#'
+#' @export
+template <- function(...) {
+  
+  # locations of resource files in the package
+  pkg_resource = function(...) {
+    system.file(..., package = "myrmdtemplate")
+  }
+  
+  template <- pkg_resource("rmarkdown/templates/myrmdtemplate/resources/template.html")
+
+   
+  
+   css = pkg_resource("rmarkdown/templates/myrmdtemplate/resources/styles.css")
+   header = pkg_resource("rmarkdown/templates/myrmdtemplate/resources/header.html")
+   
+   
+   
+   bookdown::html_document2(...,
+                            template = template,
+                            css = css,
+                            theme = cosmo,
+                            toc=true,
+                            toc_float=true,
+                            number_sections = FALSE
+   )
+   
+}
