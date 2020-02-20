b2ba3e0b3b4557d4d0508772f5708b99990a5e90
Aaron Conway <aaronconwayphd@gmail.com>
2020-02-20 13:29:05 -0500

    separate templates

---
+++ R/pmcc.R
@@ -0,0 +1,33 @@
+#' Output format for letter
+#'
+#' Each function is a wrapper for \code{\link[rmarkdown]{html_document}} to
+#' produce documents in ConwayLetterTemplate style.
+#' @rdname pmcc
+#' @param \dots Arguments passed to \code{\link[rmarkdown]{html_document}}.
+#'
+#' @return An R Markdown output format object.
+#'
+#'
+#' @export
+pmcc <- function(...) {
+  
+  # locations of resource files in the package
+  # pkg_resource = function(...) {
+  #   system.file(..., package = "myrmdtemplate")
+  # }
+  
+  # template <- pkg_resource("rmarkdown/templates/myrmdtemplate/resources/template.html")
+
+# template <- "https://raw.githubusercontent.com/rstudio/rmarkdown/master/inst/rmd/h/default.html"
+  
+   # css = pkg_resource("rmarkdown/templates/myrmdtemplate/resources/styles.css")
+   # header = pkg_resource("rmarkdown/templates/myrmdtemplate/resources/header.html")
+   
+   
+   
+   rmarkdown::html_document(...,
+                            template = "Extras/template.html",
+                            css = "Extras/styles.css",
+                            )
+   
+}
